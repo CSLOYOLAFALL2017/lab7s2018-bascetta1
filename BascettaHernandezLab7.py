68c325df0d907af90ed25d8f75dfb2e96a09e2bf
robertbascetta <rpbascetta@loyola.edu>
2018-03-23 11:29:15 -0400

    version 2

--- BascettaHernandezLab7.py
+++ BascettaHernandezLab7.py
@@ -1,4 +1,12 @@
+# Programmers: Bascetta / Hernandez
+# Date: 3/21/18
+# Lab 7
+
+# Import os for path exists
+
 import os
+
+# create a function to verify the user inputs a valid filename
 
 
 def verify():
@@ -8,29 +16,58 @@ def verify():
 
     print("Thank you", filename , "is a valid file name")
     return filename
+# call the function
+
 
 file_open = verify()
 
+# create a function for the max profit
 
-def maxiprofit(file_open):
+
+def maxprofit(file_open):
     movies_file = open(file_open, "r")
-    maxProfit = 0
+    maxiprofit = 0
     for line in movies_file:
         movieProf = line
         releaseDate, movieTitle, revenue, budget = movieProf.split(",")
         budget = float(budget)
         revenue = float(revenue)
         profit = revenue - budget
-        if profit > maxProfit:
-            maxProfit = profit
+        if profit > maxiprofit:
+            maxiprofit = profit
             titleMovie = movieTitle
-    print("The movie with the most profit is", titleMovie, "with $",maxProfit, "in profit")
-    return maxProfit
-maxiprofit(file_open)
+    print("The movie with the most profit is", titleMovie, "with $",maxiprofit, "in profit")
+    return maxprofit
+
+
+mostprofit(file_open)
+
+# create a function to process the file to read and write to
+
+
+def output(file_open, output_file):
+
+
+def process():
+    valid_output = input("Input a filename to output to > ")
+    while not os.path.exists(valid_output):
+        valid_output = input("Please enter a valid filename > ")
+
+    return valid_output
+
+
 
 
 def main():
     print("This program is designed to inform you of the movie with the highest profit")
+    file_open = verify()
+
+    mostprofit(file_open)
+
+    output_file = process()
+
+    output(file_open, output_file)
+
     
 
 
